c1dc9b02a67ff65c91cc0d416ef225d247ef0a9b
marcel <marcel_rodrgez@yahoo.com>
2022-01-14 11:50:13 -0500

    Finished writing tests and finished all logic in other components 7/7 on codegrade tests. still need to do interview questions.

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,13 @@
 // write your custom hook here to control your checkout form
+import {useState} from 'react'
+
+const useForm = (init) => {
+    const [values, setValues] = useState(init);
+  
+    const handleChanges = (e) => {
+      setValues({ ...values, [e.target.name]: e.target.value });
+    };
+    return [values, handleChanges]
+  }
+
+  export default useForm
